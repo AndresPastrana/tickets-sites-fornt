82772194ed76baa4ee990b477a35460cc9232e00
andresLnx <andreserluis@gmail.com>
2022-09-11 15:32:44 -0400

    Patient 0

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+// import { BrowserRouter } from 'react-router-dom'
+// import AppRoutes from './routes/AppRoutes'
+
+const App = () => {
+  const backend = process.env;
+  // eslint-disable-next-line no-lone-blocks
+  {console.log(backend)}
+  return (
+   <div>
+   App
+   </div>
+  )
+}
+
+export default App
